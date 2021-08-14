bb84af7f1e888f0b590cdf1f37c6a3773d673409
Matheus Ferreira <matheus.44300@gmail.com>
2021-08-14 12:36:01 -0300

    Added Circle Selection in Note Item

--- lib/components/note_item.dart
+++ lib/components/note_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notas/models/note_model.dart';
 
-class NoteItem extends StatelessWidget {
+class NoteItem extends StatefulWidget {
   final NoteModel note;
   final void Function()? onTap;
 
@@ -12,23 +12,79 @@ class NoteItem extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _NoteItemState createState() => _NoteItemState();
+}
+
+class _NoteItemState extends State<NoteItem> {
+  bool _isSelected = false;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+      padding: const EdgeInsets.only(top: 8.0),
       child: Container(
         width: double.maxFinite,
+        decoration: BoxDecoration(
+          color: _isSelected ? Colors.blueAccent : Colors.transparent,
+        ),
         constraints: BoxConstraints(maxHeight: 100),
-        child: InkWell(
-          onTap: onTap,
-          child: Card(
-            elevation: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                note.title != '' ? note.title! : note.description!,
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+          child: InkWell(
+            onTap: () {
+              if (_isSelected) {
+                setState(() => _isSelected = false);
+                return;
+              }
+
+              widget.onTap!();
+            },
+            onLongPress: () => setState(() => _isSelected = !_isSelected),
+            child: Card(
+              elevation: 2,
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      constraints: BoxConstraints(
+                        maxWidth: _isSelected
+                            ? MediaQuery.of(context).size.width - 70
+                            : double.maxFinite,
+                      ),
+                      child: Text(
+                        widget.note.title != ''
+                            ? widget.note.title!
+                            : widget.note.description!,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  if (_isSelected)
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: InkWell(
+                        onTap: () {
+                          setState(() => _isSelected = false);
+                        },
+                        child: Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.check,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
               ),
             ),
           ),
